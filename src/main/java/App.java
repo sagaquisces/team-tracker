e04444552a5b468b20e638eef6788b570713e4fc
Michael Dunlap <sagaquisces@gmail.com>
2017-07-07 09:57:32 -0700

    members are saved to an Array List so they can be added and then listed on the index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
@@ -12,7 +14,8 @@ public class App {
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
-      model.put("member", request.session().attribute("member"));
+      model.put("mostRecentMember", request.session().attribute("mostRecentMember"));
+      model.put("members", request.session().attribute("members"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -20,13 +23,20 @@ public class App {
     post("/members", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
+      ArrayList<Member> members = request.session().attribute("members");
+      if (members == null) {
+        members = new ArrayList<Member>();
+        request.session().attribute("members", members);
+      }
+
       String name = request.queryParams("name");
       String organization = request.queryParams("organization");
 
       Member newMember = new Member(name, organization);
-      request.session().attribute("member", newMember);
+      members.add(newMember);
+      request.session().attribute("mostRecentMember", newMember);
 
-      model.put("member", newMember);
+      model.put("mostRecentMember", newMember);
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
