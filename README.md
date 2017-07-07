1029cd7c426dc8e16b46b6d093277aff6c11bc69
Michael Dunlap <sagaquisces@gmail.com>
2017-07-07 09:36:42 -0700

    app will add member via form and display success page and link back to index page with form to add another member.

--- README.md
+++ README.md
@@ -24,6 +24,7 @@ team-tracker
     │   └── resources
     │       └── templates
     │           └── layout.vtl
+    │           ├── index.vtl
     └── test
         └── java
             ├── MemberTest.java
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -7,6 +8,28 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      model.put("member", request.session().attribute("member"));
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/members", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String name = request.queryParams("name");
+      String organization = request.queryParams("organization");
+
+      Member newMember = new Member(name, organization);
+      request.session().attribute("member", newMember);
+
+      model.put("member", newMember);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 }
